83228b6be52c82a4537ce0706496d33926268234
Kylee Tilley <kyleetilley@gmail.com>
2024-10-30 13:08:32 -0500

    Replace previous vite generated site with react powered vite site

---
+++ site/src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>
+        The Testing Book
+        <small>A Software Engineering Guide To Testing</small>
+      </h1>
+      <p>
+        Read it <a href="book">online</a>!
+      </p>
+    </>
+  );
+}
+
+export default App;
