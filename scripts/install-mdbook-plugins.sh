925def922f96ba8883769361533b098a2208ad90
Kylee Tilley <kyleetilley@gmail.com>
2024-11-02 21:43:43 -0500

    Bump mdbook-pagebreaks

--- scripts/install-mdbook-plugins.sh
+++ scripts/install-mdbook-plugins.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cargo install mdbook --version 0.4.40
-cargo install mdbook-pagebreaks --version 0.3.0
+cargo install mdbook-pagebreaks --version 0.3.1
 cargo install mdbook-embedify
 cargo install mdbook-mermaid
 cargo install mdbook-epub
